1d9e4f709a555f5a0b9ea2bcf7433e8d7c97aeba
Vita <vitatriutami@gmail.com>
2024-04-21 07:58:20 +0700

    add project

---
+++ src/main.js
@@ -0,0 +1,81 @@
+// NAV: grab everything we need
+const btnNav = document.querySelector(".mobile-menu-button");
+const sidebar = document.querySelector(".sidebar");
+
+// NAV: add our event listener for the click
+btnNav.addEventListener("click", () => {
+  sidebar.classList.toggle("-translate-x-full");
+});
+
+
+const API_ENDPOINT = "https://v1.appbackend.io/v1/rows/768NKI6qq7pq";
+const notesContainer = document.getElementById("noteCard");
+const form = document.getElementById("form");
+
+// form.addEventListener("submit", async (event) => {
+//   // avoid default behavior
+//   event.preventDefault();
+
+//   //Convention
+//   // targetkan ke form, ambil semua elemen dalam form
+//   const formData = new FormData(event.target);
+
+//   // ambil data
+//   const title = formData.get("title");
+//   const content = formData.get("content");
+
+//   await createData(title, content);
+//   location.reload();
+// });
+
+// async function createData(title, content) {
+//   const res = await fetch(API_ENDPOINT, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify([{ title, content }]),
+//   });
+//   const data = await res.json();
+//   return data;
+// }
+
+async function getAllData() {
+  const res = await fetch(API_ENDPOINT);
+  const data = await res.json();
+  return data;
+}
+
+async function buildApp() {
+  const notes = await getAllData();
+
+  notes.data.forEach((note) => {
+    const newNoteElement = document.createElement("div");
+    const newNoteTitleElement = document.createElement("h3");
+    const newNoteContentElement = document.createElement("p");
+    const newNoteDate = document.createElement("p");
+
+    newNoteTitleElement.textContent = note.title;
+    newNoteContentElement.textContent = note.content;
+
+    newNoteTitleElement.classList.add("text-xl", "font-bold");
+    newNoteElement.classList.add(
+      "border",
+      "border-transparent",
+      "p-2",
+      "bg-yellow-100",
+      "shadow-md",
+      "rounded-md",
+      "grid",
+      "grid-cols-1"
+
+      // "divide-y",
+      // "divide-gray-400"
+    );
+
+    newNoteElement.append(newNoteTitleElement, newNoteContentElement);
+    notesContainer.append(newNoteElement);
+  });
+}
+
+buildApp();
